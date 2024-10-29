6a8df1822bb45b4afa0a71a86ecaf2eceb47f802
ashish <ashishbahar987@gmail.com>
2024-10-30 00:56:39 +0530

    updated

---
+++ src/component/Helper.jsx
@@ -0,0 +1,21 @@
+export const boxes = [
+  { id: 1, content: "IT Management Services", imageUrl: "../it.webp", url:"/vo"},
+  { id: 2, content: "Data Tracking and Security", imageUrl: "../dt.webp" ,url:"/vo"},
+  { id: 3, content: "Website Development", imageUrl: "../wd.webp", url:"/vo"},
+  { id: 4, content: "Modern Technology Solution", imageUrl: "../mts.webp" ,url:"/vo"},
+  { id: 5, content: "UI/UX Design Services", imageUrl: "../uiux.webp" ,url:"/vo"},
+];
+export const clientLogos = [
+  "./client/1.png",
+  "./client/2.png",
+  "./client/3.png",
+  "./client/4.png",
+  "./client/5.png",
+  "./client/6.png",
+  "./client/7.png",
+  "./client/8.png",
+  "./client/9.png",
+  "./client/10.png",
+  "./client/11.png",
+  "./client/12.png",
+];
